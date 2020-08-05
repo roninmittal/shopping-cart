1a966dc1190cabccb307862cd459f83ba1b9c0b4
roninmittal <roninmittal525@gmail.com>
2020-08-06 00:41:39 +0530

    Updates

---
+++ precache-manifest.c9fddd7e1b417fb7cc0baf26317f45ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2df749b2d7af9c0bee1f798bf9b373",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "5248e37f26519bcddfcf",
+    "url": "/shopping-cart/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "b349b6f3a55481da03fa",
+    "url": "/shopping-cart/static/css/main.855eac14.chunk.css"
+  },
+  {
+    "revision": "5248e37f26519bcddfcf",
+    "url": "/shopping-cart/static/js/2.cafbf3ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/2.cafbf3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b349b6f3a55481da03fa",
+    "url": "/shopping-cart/static/js/main.7a7ea765.chunk.js"
+  },
+  {
+    "revision": "3c93234eb06abaa792ed",
+    "url": "/shopping-cart/static/js/runtime-main.d50ebec5.js"
+  }
+]);
